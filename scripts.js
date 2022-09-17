b6002fa301a805e24ac2a612b9c4d1176afe639a
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-09-17 10:32:59 +0200

    defined open() in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,5 @@
-const scroll = document.getElementsByTagName("main")[0];
-console.log(scroll);
+const main = document.getElementsByTagName("main")[0];
+
+function open() {
+    main.classList.add("open");
+}
