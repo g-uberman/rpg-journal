f92a63eeb1bd2bc07a2c10b92ce1c257846838a3
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-10-16 13:59:56 +0200

    added fetchPosts

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
 // SELECTORS
 
 const postList = document.getElementById("postList");
+
+// FETCH
+
+const fetchPosts = function () {
+    fetch('https://dummyjson.com/posts', {
+    method: "GET"})
+    .then((result) => result.json())
+    .then((result) => {
+        const posts = result?.posts || [];
+        console.log(result);
+        posts.forEach((post) => {
+            console.log(post);
+            console.log("test");
+            // createBox(post);
+          });
+    })
+}
+
+fetchPosts();
