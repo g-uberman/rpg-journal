fc777443f20473385e1e974e67aa158f29214d8e
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-10-16 14:00:55 +0200

    added createBox

--- scripts.js
+++ scripts.js
@@ -4,6 +4,14 @@ const postList = document.getElementById("postList");
 
 // FETCH
 
+const createBox = function (post) {
+    const postBox = document.createElement("div")
+    const postTitle = document.createElement("h3")
+    postTitle.innerText = post.title;
+    postBox.append(postTitle);
+    postList.append(postBox);
+}
+
 const fetchPosts = function () {
     fetch('https://dummyjson.com/posts', {
     method: "GET"})
@@ -14,7 +22,7 @@ const fetchPosts = function () {
         posts.forEach((post) => {
             console.log(post);
             console.log("test");
-            // createBox(post);
+            createBox(post);
           });
     })
 }
