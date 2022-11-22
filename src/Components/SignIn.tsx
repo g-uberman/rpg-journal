fbc2920678b1a9431b47e58eb8b4947f9ec6a516
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-22 18:06:12 +0100

    login screen skeleton

--- src/Components/SignIn.tsx
+++ src/Components/SignIn.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { StyledLoginContainer } from "./Styles/Login.styles";
+import { FormControl, TextField, Button } from "@mui/material";
 
 export const SignIn = () => {
-    return (
-        <>
-        SignIn
-        </>
-    );
+  return (
+    <StyledLoginContainer>
+      <h1>Zaloguj się</h1>
+      <FormControl>
+        <TextField
+          helperText=" "
+          variant="standard"
+          placeholder="adres email"
+        />
+        <TextField
+          helperText=" "
+          variant="standard"
+          placeholder="hasło"
+        />
+        <Button
+        variant="outlined"
+        size="small">
+        Zaloguj się
+        </Button>
+      </FormControl>
+    </StyledLoginContainer>
+  );
 };
---
+++ src/Components/Styles/Login.styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledLoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media screen and (min-width: 700px) {
+    padding-right: 140px;
+  }
+`;
