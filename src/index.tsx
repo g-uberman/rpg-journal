bce9d48abc7b4040118a6db6e8ddc837d1073487
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-08 14:55:08 +0100

    GlobalStyle swapped for scss

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-import { GlobalStyle } from "./Components/Styles/GlobalStyle";
+import "./styles/style.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
     <Router>
       <App />
     </Router>
