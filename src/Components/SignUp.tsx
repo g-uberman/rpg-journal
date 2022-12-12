9a109b2678a426c27a0970aac5ec3434b43b2892
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-12 13:34:59 +0100

    registration

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -1,9 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { StyledLoginContainer } from "./Styles/Login.styles";
 import { FormControl, TextField, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const USER_ALREADY_EXISTS_ERROR = "auth/email-already-in-use";
+const WEAK_PASSWORD_ERROR = "auth/weak-password";
+const INVALID_EMAIL_ERROR = "auth/invalid-email";
+
+interface ErrorProps {
+  error: boolean;
+}
+
+const noErrors: ErrorProps = {
+  error: false,
+};
 
 export const SignUp = () => {
+  const navigate = useNavigate();
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [repeatedPassword, setRepeatedPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [emailError, setEmailError] = useState(noErrors);
+  const [passwordError, setPasswordError] = useState(noErrors);
+
+  const clearErrors = () => {
+    setTimeout(() => {
+      setErrorMessage("");
+      setEmailError(noErrors);
+      setPasswordError(noErrors);
+    }, 5000);
+  };
+
+  const onRegister = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): Promise<void> => {
+    if (!registerPassword || !repeatedPassword || !registerEmail) {
+      setErrorMessage("errorAllFields");
+      setEmailError({ error: true });
+      setPasswordError({ error: true });
+      clearErrors();
+      return;
+    }
+    if (registerPassword !== repeatedPassword) {
+      setErrorMessage("errorPasswordsDifferent");
+      setPasswordError({ error: true });
+      clearErrors();
+      return;
+    }
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      // setUsername(registerEmail);
+      navigate("/signin");
+    } catch ({ code, message }) {
+      handleFirebaseError(code);
+    }
+  };
+
+  const handleFirebaseError = (code: unknown) => {
+    switch (code) {
+      case WEAK_PASSWORD_ERROR:
+        setErrorMessage("errorWeakPassword");
+        setPasswordError({ error: true });
+        break;
+      case INVALID_EMAIL_ERROR:
+        setErrorMessage("errorInvalidEmail");
+        setEmailError({ error: true });
+        break;
+      case USER_ALREADY_EXISTS_ERROR:
+        setErrorMessage("errorAlreadyExists");
+        setEmailError({ error: true });
+        break;
+      default:
+        break;
+    }
+    clearErrors();
+  };
+
   return (
     <StyledLoginContainer>
       <p style={{ marginBottom: "3em" }}>
@@ -14,18 +94,27 @@ export const SignUp = () => {
       <h1>Zarejestruj się</h1>
       <FormControl>
         <TextField
+          {...emailError}
+          // {...emailLabels}
+          onChange={(event) => setRegisterEmail(event.target.value)}
           helperText=" "
           variant="standard"
           placeholder="adres email"
           sx={{ marginTop: "1em" }}
         />
         <TextField
+          {...passwordError}
+          // {...passwordLabels}
+          onChange={(event) => setRegisterPassword(event.target.value)}
           helperText=" "
           variant="standard"
           placeholder="hasło"
           type="password"
         />
         <TextField
+          {...passwordError}
+          // {...password2Labels}
+          onChange={(event) => setRepeatedPassword(event.target.value)}
           helperText=" "
           variant="standard"
           placeholder="powtórzone hasło"
@@ -40,15 +129,13 @@ export const SignUp = () => {
             outline: "1px solid #5d534d",
             border: "1px solid white",
           }}
+          onClick={onRegister}
         >
           Zaloguj się
         </Button>
         <div style={{ height: "63.5px", paddingTop: "2.3em" }}>
-            Masz już konto? Przejdź do {" "}
-            <Link
-            to={{pathname:"/signin"}}>
-            logowania.
-            </Link>
+          Masz już konto? Przejdź do{" "}
+          <Link to={{ pathname: "/signin" }}>logowania.</Link>
         </div>
       </FormControl>
     </StyledLoginContainer>
