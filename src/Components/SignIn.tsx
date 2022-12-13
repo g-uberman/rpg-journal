fedd9b1cadcebf66c57bf41d4f8d758b90f393b0
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-13 15:27:30 +0100

    signing in set up

--- src/Components/SignIn.tsx
+++ src/Components/SignIn.tsx
@@ -4,9 +4,76 @@ import { useNavigate } from "react-router-dom";
 import { StyledLoginContainer } from "./Styles/Login.styles";
 import { FormControl, TextField, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
+const INVALID_EMAIL_ERROR = "auth/invalid-email";
+const WRONG_PASSWORD_ERROR = "auth/wrong-password";
+const USER_NOT_FOUND_ERROR = "auth/user-not-found";
+
+interface ErrorProps {
+  error: boolean;
+}
+
+const noErrors: ErrorProps = {
+  error: false,
+};
 
 export const SignIn = () => {
   const { userEmail, setEmail, party, setParty } = useContext(Context);
+  const navigate = useNavigate();
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [emailError, setEmailError] = useState(noErrors);
+  const [passwordError, setPasswordError] = useState(noErrors);
+
+  const clearErrors = () => {
+    setTimeout(() => {
+      setErrorMessage("");
+      setEmailError(noErrors);
+      setPasswordError(noErrors);
+    }, 5000);
+  };
+
+  const handleLogin = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): Promise<void> => {
+    if (!login || !password) {
+      setErrorMessage("errorAllFields");
+      setEmailError({ error: true });
+      setPasswordError({ error: true });
+      clearErrors();
+      return;
+    }
+    try {
+      await signInWithEmailAndPassword(auth, login, password);
+      setEmail(login);
+      navigate("/party");
+    } catch ({ code, message }) {
+      handleFirebaseError(code);
+    }
+  };
+
+  const handleFirebaseError = (code: unknown) => {
+    switch (code) {
+      case INVALID_EMAIL_ERROR:
+        setErrorMessage("errorInvalidEmail");
+        setEmailError({ error: true });
+        break;
+      case WRONG_PASSWORD_ERROR:
+        setErrorMessage("errorIncorrectPassword");
+        setPasswordError({ error: true });
+        break;
+      case USER_NOT_FOUND_ERROR:
+        setErrorMessage("errorNoAccount");
+        setEmailError({ error: true });
+        break;
+      default:
+        break;
+    }
+    clearErrors();
+  };
 
   return (
     <StyledLoginContainer>
@@ -18,14 +85,20 @@ export const SignIn = () => {
       <h1>Zaloguj się</h1>
       <FormControl>
         <TextField
+          {...emailError}
+          // {...emailLabels}
           InputProps={{ className: "login" }}
+          onChange={(event) => setLogin(event.target.value)}
           helperText=" "
           variant="standard"
           placeholder="adres email"
           sx={{ marginTop: "1em" }}
         />
         <TextField
+          {...passwordError}
+          // {...passwordLabels}
           InputProps={{ className: "login" }}
+          onChange={(event) => setPassword(event.target.value)}
           helperText=" "
           variant="standard"
           placeholder="hasło"
@@ -43,6 +116,7 @@ export const SignIn = () => {
             outline: "1px solid #5d534d",
             border: "1px solid white",
           }}
+          onClick={handleLogin}
         >
           Zaloguj się
         </Button>
