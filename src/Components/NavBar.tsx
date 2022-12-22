9e5a577ebb086c2159b6074c65e1e7a13cfa77de
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-12-22 08:30:54 +0100

    NavBat styled-components removed

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { StyledNavBar } from "./Styles/NavBar.styles";
 import { Context } from "../ContextProvider";
 
 export const PartyHeader = () => {
