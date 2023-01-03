cfe128513af64141313ed8fc7d2733efcd90bcc3
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2023-01-03 12:07:12 +0100

    password reset feedback view

--- src/Components/ResetPassword.tsx
+++ src/Components/ResetPassword.tsx
@@ -2,16 +2,13 @@ import { useState, useContext, useEffect } from "react";
 import { Context } from "./../ContextProvider";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import {
-  FormControl,
-  Input,
-  Button,
-} from "@mui/material";
+import { FormControl, Input, Button } from "@mui/material";
 
 export const ResetPassword = () => {
   const { userEmail } = useContext(Context);
   const navigate = useNavigate();
   const [login, setLogin] = useState("");
+  const [reset, setReset] = useState(false);
 
   // force out logged users
   useEffect(() => {
@@ -24,6 +21,7 @@ export const ResetPassword = () => {
     event: React.MouseEvent<HTMLButtonElement> | null
   ): Promise<void> => {
     // FIREBASE PASSWORD HANDLING NEEDS TO GO HERE
+    setReset(true);
   };
 
   const handleEnter = (
@@ -42,33 +40,42 @@ export const ResetPassword = () => {
         assumenda id illum excepturi?
       </p>
       <h1>Reset password</h1>
-      <FormControl>
-          <Input
-            id="loginEmail"
-            name="email"
-            autoComplete="username"
-            className="auth"
-            placeholder="adres email"
-            onChange={(event) => setLogin(event.target.value)}
-            onKeyDown={(event) => handleEnter(event)}
-            type="text"
-          />
-        </FormControl>
-        <Button
-          variant="contained"
-          size="small"
-          sx={{
-            maxWidth: "10em",
-            borderRadius: "0",
-            outline: "1px solid #5d534d",
-            border: "1px solid white",
-            marginTop: "1.5em",
-            marginBottom: "2.25em",
-          }}
-          onClick={handleReset}
-        >
-          Zresetuj hasło
-        </Button>
+      {!reset && (
+        <div>
+          <FormControl>
+            <Input
+              id="loginEmail"
+              name="email"
+              autoComplete="username"
+              className="auth"
+              placeholder="adres email"
+              onChange={(event) => setLogin(event.target.value)}
+              onKeyDown={(event) => handleEnter(event)}
+              type="text"
+            />
+          </FormControl>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{
+              maxWidth: "10em",
+              borderRadius: "0",
+              outline: "1px solid #5d534d",
+              border: "1px solid white",
+              marginTop: "1.5em",
+              marginBottom: "2.25em",
+            }}
+            onClick={handleReset}
+          >
+            Zresetuj hasło
+          </Button>
+        </div>
+      )}
+      {reset && (
+        <div className="loginAnnotations">
+          Nowe hasło zostało wysłane na adres email.
+        </div>
+      )}
       <div className="loginAnnotations">
         Wróć do <Link to={{ pathname: "/signin" }}>logowania.</Link>
       </div>
