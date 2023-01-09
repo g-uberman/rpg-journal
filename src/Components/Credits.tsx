540871ca2e6360325b139d97f67c1ad29263f83f
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2023-01-09 20:41:00 +0100

    code cleanup

--- src/Components/Credits.tsx
+++ src/Components/Credits.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const Credits = () => {
     return (
         <div id="credits">
--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { Context } from "./../ContextProvider";
 import { useNavigate } from "react-router-dom";
 import {
