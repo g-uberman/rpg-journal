8a10eb263730f5bec0544348e8fe524c003b88bd
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2023-01-11 15:48:34 +0100

    user panel work started

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from "react";
 import { Context } from "../ContextProvider";
-import { Button } from "@mui/material";
+import { Button, FormControl, Input } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { firebaseAuth } from "..";
 import { signOut } from "firebase/auth";
@@ -33,17 +33,52 @@ export const Settings = () => {
       )}
       <p>
         Amet consectetur, adipisicing elit. Molestiae culpa sequi assumenda
-        cumque cum accusamus dolore. Corrupti deleniti, iure facere molestiae
-        expedita ratione laudantium minima saepe assumenda id illum excepturi?
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-        nulla aliquid mollitia corrupti et. Esse dolorum asperiores quam sint
-        velit praesentium architecto, culpa quo voluptas maiores nisi magnam
-        ullam explicabo. Lorem ipsum, dolor sit amet consectetur adipisicing
-        elit. Atque nihil enim ducimus quas neque. Mollitia, possimus? Nam
-        atque, maxime amet dolores repudiandae voluptates, voluptatum
-        consequuntur impedit eligendi laborum adipisci animi. Lorem ipsum dolor
-        sit amet consectetur adipisicing elit.
+        cumque cum accusamus dolore.
       </p>
+      <div className="flexColumn">
+        <FormControl>
+          <h4>Adres e-mail:</h4>
+          <Input
+            id="loginEmail"
+            className="auth"
+            type="text"
+            value={userEmail}
+            disabled
+            sx={{
+              maxWidth: "250px",
+              marginTop: "0"
+            }}
+          />
+        </FormControl>
+        <FormControl>
+          <h4>Imię:</h4>
+          <Input
+            id="displayName"
+            className="auth"
+            type="text"
+            sx={{
+              maxWidth: "250px",
+              marginTop: "0"
+            }}
+          />
+        </FormControl>
+      </div>
+      <Button
+        variant="contained"
+        size="small"
+        sx={{
+          maxWidth: "10em",
+          borderRadius: "0",
+          outline: "1px solid #5d534d",
+          border: "1px solid white",
+          margin: "1em 0 3em"
+        }}
+        onClick={() => {
+          handleLogOut();
+        }}
+      >
+        Wyloguj
+      </Button>
       <h1>Ustawienia drużyny</h1>
       <p>
         Ea, quis quas architecto eos quaerat velit pariatur, alias, eius ipsam
@@ -53,21 +88,6 @@ export const Settings = () => {
         consequatur voluptatum nihil maiores! Quidem nobis consequatur delectus
         vel possimus. Ipsam ratione numquam exercitationem facilis.
       </p>
-      <Button
-        variant="contained"
-        size="small"
-        sx={{
-          maxWidth: "10em",
-          borderRadius: "0",
-          outline: "1px solid #5d534d",
-          border: "1px solid white",
-        }}
-        onClick={() => {
-          handleLogOut();
-        }}
-      >
-        Wyloguj
-      </Button>
     </>
   );
 };
